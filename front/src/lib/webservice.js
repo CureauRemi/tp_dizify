43e4bdaf472929c80b2fb1eacac655faa54d95a2
jlefeuvr <jordanlefeuvre@hotmail.com>
2020-11-12 16:17:33 +0100

    FEAT FRONT: Add | Delete | Put Artist Done

--- front/src/lib/webservice.js
+++ front/src/lib/webservice.js
@@ -1,20 +1,42 @@
 const axios = require('axios');
-axios.defaults.baseURL = 'localhost:8080';
+axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 module.exports = {
 
     async getAlbums(){
-        await axios.get('/album', {headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
-            "Access-Control-Allow-Headers": "Access-Control-*, Origin, X-Requested-With, Content-Type, Accept",
-            "Access-Control-Expose-Headers": "Access-Control-*",
-            'Allow': 'GET, POST, PUT, DELETE, OPTIONS, HEAD'
-          }})
+        return await axios.get('/albums?page=0&limit=25')
+            .then(function (response) {
+                return response.data.content
+            });
+    },
+
+    async getArtists(){
+        return await axios.get('/artists?page=0&nb=25')
             .then(function (response) {
                 console.log(response)
+                return response.data.content
+            });
+    },
+
+    async addArtist(body){
+        return await axios.post('/artist', body)
+            .then(function (response) {
+                return response
+            });
+    },
+
+    async deleteArtist(id){
+        return await axios.delete('/artist/' + id)
+            .then(function (response) {
+                return response
+            });
+    },
+
+    async updateArtist(artist){
+        return await axios.put('/artist/', artist)
+            .then(function (response) {
                 return response
             });
     }
